7c066fbe9d5e97c6a57ffe172f8cb3639d01a171
Yi Lu <luyi0619@users.noreply.github.com>
2014-10-16 19:50:23 +0800

    Update run_distributed.sh

--- bfs/spark/run_distributed.sh
+++ bfs/spark/run_distributed.sh
@@ -1,7 +1,7 @@
 /Users/ylu/spark/bin/spark-submit \
     --class quegel.bfs \
     --master spark://master:7077 \
-    --executor-memory 480G \
+    --executor-memory 40G \
     --total-executor-cores 120 \
     target/scala-2.10/quegel-project_2.10-1.0.jar \
     spark://master:7077 \
