2ca5f0d590342c847040d9cc5bc2aae81f6cd74a
Yi Lu <luyi0619@outlook.com>
2014-10-25 16:29:40 +0800

    update some code in spark

--- bfs/spark/run_distributed.sh
+++ bfs/spark/run_distributed.sh
@@ -1,10 +1,10 @@
-/Users/ylu/spark/bin/spark-submit \
+/usr/local/spark/bin/spark-submit \
     --class quegel.bfs \
     --master spark://master:7077 \
-    --executor-memory 40G \
-    --total-executor-cores 120 \
+    --executor-memory 45G \
+    --total-executor-cores 345 \
     target/scala-2.10/quegel-project_2.10-1.0.jar \
     spark://master:7077 \
-    hdfs://master:9000/dg.txt \
-    hdfs://master:9000/result \
-    biBfs
+    hdfs://master:9000/ylu/btc.txt \
+    hdfs://master:9000/ylu/output/btcbfs \
+    bfs
